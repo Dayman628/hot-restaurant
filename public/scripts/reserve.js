57a4840af03b8213a3c3b7f59a67c15e4c96820c
Tanner Nielsen <tan-ali5@outlook.com>
2020-06-13 13:22:42 -0700

    moved js to own file reserve

--- public/scripts/reserve.js
+++ public/scripts/reserve.js
@@ -1,1 +1,15 @@
 // create js
+$(".submit").on("click", function(event) {
+    event.preventDefault();
+
+    const newReservation = {
+        costomerName: $("#reserve-name").val().trim(),
+        costomerNumber: $("#reserve-phone").val().trim(),
+        costomerEmail: $("#reserve-email").val().trim(),
+        costomerID: $("#reserve-number").val().trim()
+    };
+
+    console.log(newReservation);
+
+})
+
